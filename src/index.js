db3bc0311d35379abc57486e11169f7ecb3a0a50
Valeria Koroleva <valeriia.koroleva@gmail.com>
2021-03-11 23:03:46 +0300

    feat: check balanced brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arr = str.split('')
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let c = 0; c < bracketsConfig.length; c++) {
+            if (arr[i-1] === bracketsConfig[c][0] && arr[i] === bracketsConfig[c][1]){
+                arr.splice(i-1, 2)
+                i = 0
+                break
+            }
+        }
+    }
+
+    return arr.length === 0
 }
